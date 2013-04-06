5764188590c04c21977cc136cc27ac0cce139c4c
wayne <wayne@155.41.105.3>
2013-04-06 15:19:07 -0400

    dump

---
+++ src/ibalgo/algorithm/Hearding.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ibalgo.algorithm;
+
+/**
+ *
+ * @author wayne
+ */
+public class Hearding {
+    
+}
---
+++ src/ibalgo/communication/IBShell.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ibalgo.communication;
+
+/**
+ *
+ * @author wayne
+ */
+public class IBShell {
+    
+    public static double getLast(String ticker) {
+        return 0;
+    }
+}
